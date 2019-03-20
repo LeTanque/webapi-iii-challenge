ab701635961f2d72008091a459abd08fe4c5af58
Frank Martinez <frankie.martinez@gmail.com>
2019-03-20 14:28:20 -0700

    get, getbyid, post, delete all working for posts now. Improved error messaging and edge case resolutions

--- data/routesPosts.js
+++ data/routesPosts.js
@@ -19,6 +19,85 @@ routesPosts.get('/', async (req, res) => {
 });
 
 
+// Get single post by id
+routesPosts.get('/:id', async (req, res) => {
+    try {
+        const postById = await PostDB.getById(req.params.id);
+        if (postById.length === 0) {  
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        } else {
+            res.status(200).json(postById);
+        }
+    } 
+    catch (error) { 
+        res.status(500).json({ error: "The post could not be retrieved." });
+    }
+});
+
+
+// Add a new post as user by id
+routesPosts.post('/', async (req, res) => {
+    if (!req.body.text) {
+        return res.status(409).json({ message: "Post cannot be blank!" });
+    }
+    if (!req.body.user_id) {
+        return res.status(409).json({ message: "Please include user id." });
+    }
+    try {
+        const data = await PostDB.insert(req.body);
+        res.status(200).json(`Successfully added post as user_id: ${data.user_id}`);
+    }
+    catch (error) {
+        res.status(500).json({ error: "Error adding post!" });
+    }
+});
+
+
+// Delete a post from the db
+routesPosts.delete('/:id', async (req, res) => {
+    const checkPostExists = await PostDB.getById(req.params.id);
+    if (!checkPostExists) { 
+        return res.status(404).json({ message: "The post with ID does not exist. Cannot delete!" });
+    }
+    try {
+        const postDeleted = await PostDB.remove(req.params.id);
+        if (postDeleted > 1) {
+            res.status(200).json({ message: `${postDeleted} posts have been deleted` })
+        } else {
+            res.status(200).json({ message: `${postDeleted} post has been deleted` })
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: "Something went wrong deleting post!"})
+    }
+});
+
+
+// // Update user name
+// routesPosts.put('/:id', async (req, res) => {
+//     try {
+//         const checkUserExists = await PostDB.getById(req.params.id);
+//         const allUsers = await PostDB.get(req.query);
+//         const userFiltered = allUsers.filter(user => user.name === req.body.name);
+//         if (!checkUserExists) { 
+//             return res.status(404).json({ message: "The user with the specified ID does not exist. Cannot update!" });
+//         } else if (!req.body.name) {
+//             return res.status(200).json({ message: "Nothing updated." })
+//         } else if (userFiltered.length >= 1) {
+//             return res.status(409).json({ message: "Please choose a different name. User already exists." })
+//         }
+//     }
+//     catch (error) {
+//         res.status(500).json({ message: "Something went wrong updating user!"})
+//     }
+//     try {
+//         const userUpdate = await PostDB.update(req.params.id, req.body);
+//         res.status(200).json({ message: `${userUpdate} user has been updated` })
+//     }
+//     catch (error) {
+//         res.status(500).json({ message: "Something went wrong updating user!"})
+//     }
+// });
 
 
 
